7d16686eb63c5edc28dcf2aa75812e8156a85e51
lesley <lesleybanadzem2017@gmail.com>
2025-02-11 20:23:48 -0600

    Add User model with MongoDB ObjectID and authentication fields

---
+++ models/userModels.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID            primitive.ObjectID `bson:"_id"`
+	Email         *string            `json:"email" validate:"required,min=4,max=100"`
+	Password      *string            `json:"password" validate:"required,min=8"`
+	Token         *string            `json:"token"`
+	Refresh_token *string            `json:"refresh_token"`
+	Created_at    time.Time          `json:"created_at"`
+	Updated_at    time.Time          `json:"updated_at"`
+	User_id       string             `json:"user_id"`
+}
