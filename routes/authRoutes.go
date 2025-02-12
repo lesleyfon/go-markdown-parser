283a353d865564151f33bf01884324bd1283347c
lesley <lesleybanadzem2017@gmail.com>
2025-02-11 20:27:25 -0600

    Add authentication routes for user signup endpoint

---
+++ routes/authRoutes.go
@@ -0,0 +1,10 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"main.go/controller"
+)
+
+func AuthRoutes(router *gin.Engine) {
+	router.POST("/auth/v1/signup", controller.SignUpController())
+}
