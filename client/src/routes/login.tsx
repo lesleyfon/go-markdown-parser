349a3bd6a6f634a3e24b76a61fc36a48fa2af777
lesley <lesleybanadzem2017@gmail.com>
2025-02-12 08:09:38 -0600

    Implement login functionality with React Query and error handling

--- client/src/routes/login.tsx
+++ client/src/routes/login.tsx
@@ -1,6 +1,7 @@
 import z from "zod";
 import { createFileRoute } from "@tanstack/react-router";
 import { useForm } from "@tanstack/react-form";
+import { useMutation, useQueryClient, UseMutationResult } from "@tanstack/react-query";
 
 export const Route = createFileRoute("/login")({
 	component: Login,
@@ -13,6 +14,46 @@ const emailPasswordValidator = z.string().min(6, {
 	message: "Password must be of length 6 or greater",
 });
 
+async function loginFn({ email, password }: { email: string; password: string }) {
+	const myHeaders = new Headers();
+	myHeaders.append("Content-Type", "application/json");
+
+	const requestOptions = {
+		method: "POST",
+		headers: myHeaders,
+		body: JSON.stringify({
+			email,
+			password,
+		}),
+		redirect: "follow" as RequestRedirect,
+	};
+
+	const response = await fetch("http://0.0.0.0:8080/auth/v1/login", requestOptions);
+
+	if (!response.ok) {
+		const error = await response.json();
+		throw new Error(error.message);
+	}
+
+	const data = await response.json();
+
+	return data;
+}
+
+function AuthenticationStatus({
+	mutation,
+}: {
+	mutation: UseMutationResult<unknown, Error, { email: string; password: string }, unknown>;
+}) {
+	return (
+		<div>
+			<p className="text-blue-300 text-left">{mutation.isPending && "Loading..."}</p>
+			<p className="text-red-300 text-left">{mutation.isError && mutation.error.message}</p>
+			<p className="text-green-300 text-left">{mutation.isSuccess && "Success"}</p>
+		</div>
+	);
+}
+
 export default function Login() {
 	const form = useForm({
 		defaultValues: {
@@ -20,6 +61,17 @@ export default function Login() {
 			password: "",
 		},
 	});
+
+	// Access the client
+	const queryClient = useQueryClient();
+
+	const mutation = useMutation({
+		mutationFn: loginFn,
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ["login"] });
+		},
+	});
+
 	return (
 		<div>
 			<h1>login</h1>
@@ -27,8 +79,11 @@ export default function Login() {
 				onSubmit={(e) => {
 					e.preventDefault();
 					e.stopPropagation();
-					console.log(form.getFieldValue("email"));
-					console.log(form.getFieldValue("password"));
+
+					mutation.mutate({
+						email: form.getFieldValue("email"),
+						password: form.getFieldValue("password"),
+					});
 					form.handleSubmit();
 				}}
 				className=" flex flex-col gap-2"
@@ -48,7 +103,9 @@ export default function Login() {
 								onBlur={field.handleBlur}
 								onChange={(e) => field.handleChange(e.target.value)}
 							/>
-							<em role="alert">{field.state.meta.errors.join(", ")} </em>
+							<em role="alert" className=" text-red-300 text-left">
+								{field.state.meta.errors.join(", ")}{" "}
+							</em>
 						</>
 					)}
 				/>
@@ -67,11 +124,14 @@ export default function Login() {
 								onBlur={field.handleBlur}
 								onChange={(e) => field.handleChange(e.target.value)}
 							/>
-							<em role="alert">{field.state.meta.errors.join(", ")} </em>
+							<em role="alert" className=" text-red-300 text-left">
+								{field.state.meta.errors.join(", ")}{" "}
+							</em>
 						</>
 					)}
 				/>
 				<button type="submit">Submit</button>
+				<AuthenticationStatus mutation={mutation} />
 			</form>
 		</div>
 	);
