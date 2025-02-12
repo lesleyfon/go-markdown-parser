1e3ab321fdb816b4744720ada2e5b89e216d2eb5
lesley <lesleybanadzem2017@gmail.com>
2025-02-11 20:56:24 -0600

    Add token update utility function for user authentication

--- utils/tokenUtils.go
+++ utils/tokenUtils.go
@@ -1,12 +1,20 @@
 package utils
 
 import (
+	"context"
 	"log"
 	"os"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"main.go/database"
+	"main.go/models"
 )
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
 
 type JwtSignedDetails struct {
 	Email     string
@@ -74,4 +82,57 @@ func GenerateAllTokens(
 	// Return the token and refresh token
 	return token, refreshToken, err
 
+}
+
+func UpdateTokens(signedToken string, signedRefreshedToken string, userId string) (*models.User, error) {
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+
+	// Initialize the update document
+	updateTokenDocs := bson.D{
+		{
+			Key:   "token",
+			Value: signedToken,
+		},
+		{
+			Key:   "refreshedToken",
+			Value: signedRefreshedToken,
+		},
+		{
+			Key:   "updated_at",
+			Value: time.Now(),
+		},
+	}
+
+	// Define filter based on the userId
+	filter := bson.M{"user_id": userId}
+	returnDocument := options.After
+
+	// Specify options for upsert and to return the updated document
+	upsert := true
+	opt := options.FindOneAndUpdateOptions{
+		Upsert:         &upsert,
+		ReturnDocument: &returnDocument, // Return the updated document
+	}
+
+	// Perform the update operation and get the updated document back
+	var updatedUser models.User
+	err := userCollection.FindOneAndUpdate(
+		ctx,
+		filter,
+		bson.D{
+			{
+				Key:   "$set",
+				Value: updateTokenDocs,
+			},
+		},
+		&opt,
+	).Decode(&updatedUser) // Decode the result into updatedUser
+
+	if err != nil {
+		log.Printf("Error updating token for user %s: %v", userId, err.Error())
+		return nil, err
+	}
+
+	return &updatedUser, nil
 }
