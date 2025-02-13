1b2c4e15ecb478ff03df4f39a632fc99d8d2ee7e
lesley <lesleybanadzem2017@gmail.com>
2025-02-12 18:17:17 -0600

    Add document file type validator for Markdown files

--- client/src/lib/formValidations.tsx
+++ client/src/lib/formValidations.tsx
@@ -5,3 +5,7 @@ export const emailValidator = z.string().email("Invalid Email").min(2, {
 export const passwordValidator = z.string().min(6, {
 	message: "Password must be of length 6 or greater",
 });
+
+export const documentValidator = z
+	.instanceof(File)
+	.refine((file) => ["text/markdown"].includes(file.type), { message: "Invalid file type" });
