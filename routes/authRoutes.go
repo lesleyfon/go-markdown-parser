e8d2f4dfc5527038fcede5f360f5cf1e2fa87db1
lesley <lesleybanadzem2017@gmail.com>
2025-02-13 22:33:35 -0600

    Add user authentication route - Implement GET endpoint for user authentication

--- routes/authRoutes.go
+++ routes/authRoutes.go
@@ -8,4 +8,7 @@ import (
 func AuthRoutes(router *gin.Engine) {
 	router.POST("/auth/v1/signup", controller.SignUpController())
 	router.POST("/auth/v1/login", controller.LogIn())
+
+	// route to authenticate user
+	router.GET("/auth/v1/authenticate", controller.AuthenticateUser())
 }
