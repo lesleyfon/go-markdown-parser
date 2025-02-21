af5b5598cd2bc11e22c68a52da10f0f11f2936aa
lesley <lesleybanadzem2017@gmail.com>
2025-02-20 22:26:43 -0600

    Add fetchFile API function for retrieving markdown file details

---
+++ client/src/apis/fetchFile.ts
@@ -0,0 +1,26 @@
+import { API_BASE_URL } from "@/lib/constants";
+import { FileResponse } from "@/routes/files/$file-id";
+
+export async function fetchFile(fileId: string): Promise<FileResponse> {
+	const token = localStorage.getItem("auth-token");
+	if (!token) {
+		return {
+			message: "No token found, please login again",
+			status: 401,
+		};
+	}
+
+	const res = await fetch(`${API_BASE_URL}/api/v1/markdown/files/${fileId}`, {
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	});
+
+	if (!res.ok) {
+		const error = await res.json();
+		throw new Error(error.message);
+	}
+
+	const data = await res.json();
+	return data;
+}
