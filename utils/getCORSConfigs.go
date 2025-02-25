13dfe2ba39dba91a0243efe3a655b459d28de828
lesley <lesleybanadzem2017@gmail.com>
2025-02-24 21:36:23 -0600

    Add utility function for dynamic CORS origin configuration

---
+++ utils/getCORSConfigs.go
@@ -0,0 +1,31 @@
+package utils
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+)
+
+func GetCorsOrigins() []string {
+	// Default origins for development
+	var CORS_ALLOWED_ORIGINS []string
+
+	// Try to get origins from env
+	ALLOW_ORIGINS_JSON := os.Getenv("ALLOW_ORIGINS")
+	if ALLOW_ORIGINS_JSON == "" {
+		log.Println("Warning: ALLOW_ORIGINS not set, using default origins")
+		return CORS_ALLOWED_ORIGINS
+	}
+
+	if err := json.Unmarshal([]byte(ALLOW_ORIGINS_JSON), &CORS_ALLOWED_ORIGINS); err != nil {
+		log.Printf("Error parsing CORS origins: %v. Using default origins", err)
+		return CORS_ALLOWED_ORIGINS
+	}
+
+	if len(CORS_ALLOWED_ORIGINS) == 0 {
+		log.Println("Warning: No CORS origins provided, using default origins")
+		return CORS_ALLOWED_ORIGINS
+	}
+
+	return CORS_ALLOWED_ORIGINS
+}
